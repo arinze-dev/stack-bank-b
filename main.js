8058055921636fcb4d1f402ec6320dae2eed6fcf
Anthony_dev <atozbasic@gmail.com>
2022-10-15 11:12:15 +0100

    added access control

--- main.js
+++ main.js
@@ -7,10 +7,6 @@ require("dotenv").config();
 
 let PORT = process.env.PORT  || 4040
 
-
-
-
-const cors=require("cors");
 const corsOptions ={
    origin:'*', 
    credentials:true,            //access-control-allow-credentials:true
@@ -25,11 +21,12 @@ app.use(cors(corsOptions))
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 // const connection = process.env.NODE_ENV == 'production' ? 'mongodb://127.0.0.1:27017/StackDB':process.env.DB_ATLAS_LINK;
-
 
 mongoose.connect(process.env.DB_ATLAS_LINK, () =>
   console.log("connected to StackDB")
@@ -38,18 +35,11 @@ mongoose.connect(process.env.DB_ATLAS_LINK, () =>
 const { UserRouter } = require("./route/user-route.js");
 const { TransactionRouter } = require("./route/transaction.js");
 
-
-
 // Route Middleware
 app.use("/api/user", UserRouter);
 app.use("/api/tx", TransactionRouter);
 
-
-
-
-
 //  const atlasDB = `mongodb+srv://pseudobrains:test12345@cluster0.xo0lnsr.mongodb.net/stackDB?retryWrites=true&w=majority`;
-
 
 // mongoose.connect(atlasDB,{
 //    useNewUrlParser:true,
@@ -59,21 +49,12 @@ app.use("/api/tx", TransactionRouter);
 //   useFindAndModify:false
 // } 
 
-
-
 app.get("/", (req,res) => {
   res.send({message : "welcome to  stak bank"});
 });
 
-
 app.get("/api", (req,res) => {
     res.send({message : "welcome to  stak bank"});
 });
-
-
-// if(process.env.NODE_ENV === "production"){
-
-// }
-
 
 app.listen(PORT, () => console.log("server is runing"));
